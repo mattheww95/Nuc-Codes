cc63827882ab0e55cfdf673a0b0dbb5fde8f2c05
Matthew Wells <matthew.wells@canada.ca>
2021-07-30 12:19:02 -0500

    Added find packages to setup

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="nuc_codes",
     author="Matthew Wells",
+    packages=find_packages(),
     author_email="matthew.wells@canada.ca",
     entry_points={"console_scripts": ["nuc_codes=nuc_codes.nuc_codes:control_gff_finder"]},
 )
