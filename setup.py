b144e153e65808e088033e51a8347169e671bb10
Matthew Wells <matthew.wells@canada.ca>
2021-07-30 12:37:08 -0500

    Testing new entry point

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     author="Matthew Wells",
     packages=find_packages(),
     author_email="matthew.wells@canada.ca",
-    entry_points={"console_scripts": ["nuc_codes=nuc_codes.nuc_codes:main"]},
+    scripts=["nuc_codes=nuc_codes/nuc_codes.py"],
 )
